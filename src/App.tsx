8fc184e5e1391b2a88aa3edefc1515d870f8ea60
Fe. Pastore <fesp.dotto@gmail.com>
2022-10-13 21:46:59 -0300

    Configuração de Componentes/StoryBook

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold text-2xl text-black'>Hello World</h1>
+      <button className="bg-cyan-500">Enviar</button>
+    </>
+  )  
+}
+
+
